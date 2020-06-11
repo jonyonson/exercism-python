eb9518ec173dd1fc408fd98ac86315cef5a526dc
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-11 14:52:58 -0500

    move matrix transformation from strings to ints to init

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,12 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
+        self.matrix = self.__matrix()
 
     def row(self, index):
-        return self.__matrix()[index - 1]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        return [row[index - 1] for row in self.__matrix()]
+        return [row[index - 1] for row in self.matrix]
 
     def __matrix(self):
         str_lines = self.matrix_string.splitlines()
