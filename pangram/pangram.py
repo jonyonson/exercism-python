a28e5ab6248e504273b19d5bf1621efcc6655b5a
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-14 17:51:18 -0500

    change alphabet from list to str

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,12 +1,7 @@
 def is_pangram(sentence):
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     for char in sentence.lower():
-        if not alphabet:
-            return True
-
-        if char in alphabet:
-            alphabet.remove(char)
+        alphabet = alphabet.replace(char, "")
 
     return bool(not alphabet)
