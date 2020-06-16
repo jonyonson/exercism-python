8a2c404582a9a776169eb3bdf9049c7e622413c0
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-16 10:04:08 -0500

    space-age: refactor

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -3,32 +3,43 @@ class SpaceAge:
     def __init__(self, seconds):
         self.seconds = seconds
         self.seconds_in_year = 31557600
+        self.orbits_per_earth_year = {
+            'earth': 1,
+            'mercury': 0.2408467,
+            'venus': 0.61519726,
+            'mars': 1.8808158,
+            'jupiter': 11.862615,
+            'saturn': 29.447498,
+            'uranus': 84.016846,
+            'neptune': 164.79132,
+        }
 
-    def __format_years(self, orbit_earth_years):
-        years = self.seconds / (self.seconds_in_year * orbit_earth_years)
+    def __format_age(self, planet):
+        years = self.seconds / (self.seconds_in_year *
+                                self.orbits_per_earth_year[planet])
         formatted_year = f'{years:.2f}'
         return float(formatted_year)
 
     def on_earth(self):
-        return self.__format_years(1)
+        return self.__format_age('earth')
 
     def on_mercury(self):
-        return self.__format_years(0.2408467)
+        return self.__format_age('mercury')
 
     def on_venus(self):
-        return self.__format_years(0.61519726)
+        return self.__format_age('venus')
 
     def on_mars(self):
-        return self.__format_years(1.8808158)
+        return self.__format_age('mars')
 
     def on_jupiter(self):
-        return self.__format_years(11.862615)
+        return self.__format_age('jupiter')
 
     def on_saturn(self):
-        return self.__format_years(29.447498)
+        return self.__format_age('saturn')
 
     def on_uranus(self):
-        return self.__format_years(84.016846)
+        return self.__format_age('uranus')
 
     def on_neptune(self):
-        return self.__format_years(164.79132)
+        return self.__format_age('neptune')
