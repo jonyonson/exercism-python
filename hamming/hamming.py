05d572baad6255cc33c8a204dbeb19f27296cdfd
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-16 09:54:01 -0500

    hamming: convert while loop to list comp

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,11 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("DNA strands must have the same length")
 
-    hamming_distance = 0
-    i = 0
-    while(i < len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            hamming_distance += 1
-        i += 1
-
-    return hamming_distance
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
