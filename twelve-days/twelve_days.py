4b31060d0285f83e8eedd6523f39972150e9a997
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-24 11:47:26 -0500

    twelve days: refactor

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,17 +1,18 @@
+ORDINALS = ['first', 'second', 'third', 'fourth', 'fifth',
+            'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
+
+GIFTS = ['and a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens', 'four Calling Birds', 'five Gold Rings', 'six Geese-a-Laying',
+         'seven Swans-a-Swimming', 'eight Maids-a-Milking', 'nine Ladies Dancing', 'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming']
+
+
 def recite(start_verse, end_verse):
-    ordinal_numbers = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth',
-                       'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
+    return [verse(i) for i in range(start_verse, end_verse + 1)]
 
-    gifts = [' and a Partridge in a Pear Tree', ' two Turtle Doves', ' three French Hens', ' four Calling Birds', ' five Gold Rings', ' six Geese-a-Laying',
-             ' seven Swans-a-Swimming', ' eight Maids-a-Milking', ' nine Ladies Dancing', ' ten Lords-a-Leaping', ' eleven Pipers Piping', ' twelve Drummers Drumming']
 
-    lyrics = []
-    for i in range(start_verse, end_verse + 1):
-        if i == 1:
-            end_of_verse = ' a Partridge in a Pear Tree'
-        else:
-            end_of_verse = ','.join(gifts[i - 1::-1])
-        line = f'On the {ordinal_numbers[i-1]} day of Christmas my true love gave to me:{end_of_verse}.'
-        lyrics.append(line)
+def verse(verse_num):
+    if verse_num == 1:
+        end_of_verse = 'a Partridge in a Pear Tree'
+    else:
+        end_of_verse = ', '.join(GIFTS[verse_num - 1::-1])
 
-    return lyrics
+    return f'On the {ORDINALS[verse_num-1]} day of Christmas my true love gave to me: {end_of_verse}.'
